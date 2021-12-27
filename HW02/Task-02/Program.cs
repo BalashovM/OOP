9df542b6e3e810c3ef1da1985e230929a0deb995
BalashovM <mi-h-a@mail.ru>
2021-12-28 01:40:37 +0700

    Homework02 -Finish

---
+++ HW02/Task-02/Program.cs
@@ -0,0 +1,27 @@
+﻿using HW02;
+using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+
+            for (var i=0; i<5; i++)
+            {
+                var bankAccount = new BankAccount();
+                var randomBalance = new decimal(generator.NextDouble());
+                randomBalance = randomBalance * (decimal)Math.Pow(10, 12);
+                bankAccount.SetBalance(randomBalance);
+
+                bankAccount.SetAccountType((BankAccountType)generator.Next(1, Enum.GetNames(typeof(BankAccountType)).Length));
+
+                Console.WriteLine(bankAccount.ToString());
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW02/Task-04/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace HW02
+{
+    enum BankAccountType 
+    {
+        None,
+        Payment,
+        Saving,
+        Card
+    }
+    internal class BankAccount
+    {
+        public string AccountNumber { get; set; } 
+        public decimal Balance { get; set; }
+        public BankAccountType AccountType { get; set; } 
+
+        public BankAccount()
+        {
+            AccountNumber = Guid.NewGuid().ToString();
+        }
+
+        public BankAccount(decimal balance, BankAccountType accountType):this()
+        {
+            Balance = balance;
+            AccountType = accountType;
+        }
+
+        public BankAccount(string accountNumber, decimal balance, BankAccountType accountType)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+            AccountType = accountType;
+        }
+ 
+        public override string ToString()
+        {
+            return $"Account number: {AccountNumber}\nBalance: {Balance.ToString("N1", CultureInfo.InvariantCulture)}\nAccount type: {AccountType}";
+        }
+    }
+}
